f1eae1b99c0ec96681bb2dd9e896a719e95c6fda
HiMyNameIsAdam <xgurke@gmail.com>
2021-02-17 20:16:16 +0100

    New prefabs, generate from array

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,18 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
     private double PLAYER_DIST_TO_SPAWN;
-    [SerializeField] private Transform _level;
-    [SerializeField] private Transform _level2;
-    [SerializeField] private Transform _player;
+    [SerializeField] private Transform start_level;
+    [SerializeField] private List<Transform> next_levels;
+    [SerializeField] private Transform player;
     private Vector3 _lastEnd;
     private Transform _refLastLevel;
 
     private void Awake()
     {
         PLAYER_DIST_TO_SPAWN = 15;
-        _refLastLevel = _level;
+        _refLastLevel = start_level;
     }
 
     private void SpawnLevel()
@@ -20,13 +21,13 @@ public class LevelGenerator : MonoBehaviour
         var prevPos = Vector3Int.FloorToInt(_refLastLevel.Find("EndPosition").position);
         _lastEnd = _refLastLevel.Find("EndPosition").position;
         var startPos = Vector3Int.FloorToInt(_refLastLevel.Find("StartPosition").position);
-        _refLastLevel = Instantiate(Random.Range(0.0f, 1.0f) < 0.5 ? _level : _level2, prevPos, Quaternion.identity);
+        _refLastLevel = Instantiate(next_levels[Random.Range(0, next_levels.Count)], prevPos, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_lastEnd.x - _player.position.x < PLAYER_DIST_TO_SPAWN)
+        if (_lastEnd.x - player.position.x < PLAYER_DIST_TO_SPAWN)
         {
             SpawnLevel();
         }
