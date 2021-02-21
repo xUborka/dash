52e1c75954d6cca321ccb07afe0babd87139ab7f
HiMyNameIsAdam <xgurke@gmail.com>
2021-02-21 22:32:05 +0100

    Jump fix on mobile

--- Assets/Scripts/BetterJumping.cs
+++ Assets/Scripts/BetterJumping.cs
@@ -13,13 +13,29 @@ namespace Assets.Scripts
             rb = GetComponent<Rigidbody2D>();
         }
 
+        bool TouchJump()
+        {
+            if (Input.touchCount > 0)
+            {
+                // Touch touch = Input.GetTouch(0);
+                foreach (Touch touch in Input.touches)
+                {
+                    if (touch.phase == TouchPhase.Stationary && touch.position.x < Screen.width / 2f)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         void Update()
         {
             if (rb.velocity.y < 0)
             {
                 rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
             }
-            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            else if (rb.velocity.y > 0 && (!Input.GetButton("Jump") && !TouchJump()))
             {
                 rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
             }
