5e8d8b4259541b40d67303f93cf8608d2f86e89c
Gergely Bakos <geryllaz@hotmail.com>
2021-02-21 13:51:38 +0100

    - #14 JumpPad animation

--- Assets/Scripts/JumpPadScript.cs
+++ Assets/Scripts/JumpPadScript.cs
@@ -2,10 +2,14 @@
 
 public class JumpPadScript : MonoBehaviour
 {
+    [Header("Base")]
+    [SerializeField] private float _force = 900f;
+
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -14,12 +18,21 @@ public class JumpPadScript : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<Rigidbody2D>
-                ().AddForce(new Vector2(0f, 700f));
+            _animator.SetTrigger("Launch");
+            collision.gameObject.GetComponent<Rigidbody2D>
+                ().AddForce(new Vector2(0f, _force));
         }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        //if (collision.gameObject.CompareTag("Player"))
+        //{
+        //    _animator.ResetTrigger("Launch");
+        //}
     }
 }
