370a2ad0d4e82ad02b361c04366bb6616b92caf0
xUborka <xgurke@gmail.com>
2021-03-20 19:55:22 +0100

    Test

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,6 +9,7 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Transform player;
     private Vector3 _lastEnd;
     private Transform _refLastLevel;
+    private Transform parent;
 
     public List<Transform> platform_references;
 
@@ -16,6 +17,8 @@ public class LevelGenerator : MonoBehaviour
     {
         PLAYER_DIST_TO_SPAWN = 15;
         _refLastLevel = start_level;
+        platform_references.Add(_refLastLevel);
+        parent = GameObject.Find("EndlessLevel").gameObject.transform;
     }
 
     private void SpawnLevel()
@@ -24,6 +27,7 @@ public class LevelGenerator : MonoBehaviour
         _lastEnd = _refLastLevel.Find("EndPosition").position;
         var startPos = Vector3Int.FloorToInt(_refLastLevel.Find("StartPosition").position);
         _refLastLevel = Instantiate(next_levels[Random.Range(0, next_levels.Count)], prevPos, Quaternion.identity);
+        _refLastLevel.transform.parent = parent;
         platform_references.Add(_refLastLevel);
     }
 
@@ -33,6 +37,20 @@ public class LevelGenerator : MonoBehaviour
         if (_lastEnd.x - player.position.x < PLAYER_DIST_TO_SPAWN)
         {
             SpawnLevel();
+        }
+        
+        List<Transform> to_remove = new List<Transform>();
+        foreach (var platform in platform_references)
+        {
+            Vector3 end_pos = platform.Find("EndPosition").position;
+            if (player.position.x > end_pos.x + PLAYER_DIST_TO_SPAWN)
+            {
+                to_remove.Add(platform);
+            }
+        }
+        foreach (var platform in to_remove){
+            platform_references.Remove(platform);
+            Destroy(platform.gameObject);
         }
     }
 }
